61e270d45808c425601febdcdfadac838989901c
pedro1miranda <pedromjorge2005@gmail.com>
2026-02-08 01:47:21 -0300

    Tela de faq, ajustes nas categoris de tênis, links corretos, responsabilidade

--- ajuda/script.js
+++ ajuda/script.js
@@ -38,4 +38,22 @@ overlay.addEventListener("click", (e) => {
 });
 
 closeMenuBtn.addEventListener("click", closeMenu);
-//
+
+// Mostrar respostas
+
+const faqItens = document.querySelectorAll(".faq-item");
+
+faqItens.forEach(item => {
+    item.querySelector(".faq-pergunta").addEventListener("click", () => {
+
+        const EstaAtivo = item.classList.contains("active");
+
+        faqItens.forEach(outroItem => {
+            outroItem.classList.remove("active");
+        });
+
+        if (!EstaAtivo) {
+            item.classList.add("active");
+        }
+    });
+});
